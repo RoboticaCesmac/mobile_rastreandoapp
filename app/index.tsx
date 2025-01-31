a29c34e0b2cf1c50058c37ae3a3dfc8fd40e1a5e
roboticacesmac <roboticacesmac@gmail.com>
2025-01-31 11:39:00 -0300

    Corrigindo crash ao iniciar

--- app/index.tsx
+++ app/index.tsx
@@ -21,6 +21,7 @@ export default function SplashScreenApp() {
   }, [fontsLoaded]);
 
   useEffect(() => {
+    console.log()
     const timer = setTimeout(() => {
       router.push({ pathname: '/paginaInicial' });
     }, 3500);
